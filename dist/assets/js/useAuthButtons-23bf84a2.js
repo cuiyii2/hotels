de7cd37986b234cd374bd689c97ceb415896bf73
cuiyanwei <2243007497@qq.com>
2024-10-15 10:08:33 +0800

    del

--- dist/assets/js/useAuthButtons-23bf84a2.js
+++
@@ -1,1 +0,0 @@
-import{a4 as r,a5 as s,C as a}from"./index-ab608e36.js";const B=()=>{const u=r(),o=s().authButtonListGet[u.name]||[];return{BUTTONS:a(()=>{let t={};return o.forEach(e=>t[e]=!0),t})}};export{B as u};
