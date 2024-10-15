8569dce9e3ffaf89e1095ea88f63649485dee964
cuiyanwei <2243007497@qq.com>
2024-10-15 10:50:01 +0800

    test

---
+++ admin/assets/js/useAuthButtons-CiniDLFU.js
@@ -0,0 +1,1 @@
+import{A as s,al as r,s as n}from"./index-DEpa5K7h.js";const B=()=>{const u=s(),o=r().authButtonListGet[u.name]||[];return{BUTTONS:n(()=>{let t={};return o.forEach(e=>t[e]=!0),t})}};export{B as u};
