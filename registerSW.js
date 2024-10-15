88fbc7a04d51ba031d83a8106959930ceeccf725
cuiyanwei <2243007497@qq.com>
2024-10-15 11:04:20 +0800

    test

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/hotels/sw.js', { scope: '/hotels/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
